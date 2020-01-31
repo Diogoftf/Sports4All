17d13437a404348a277ca59a4cfc5e6e1a407684
Diogo Filipe Teixeira Ferreira <2082614@student.uma.pt>
2020-01-31 18:07:47 +0000

    Update Database: price added to ground

--- Sports4All/Ground.cs
+++ Sports4All/Ground.cs
@@ -13,6 +13,7 @@ namespace Sports4All
         public virtual ICollection<Sport> Sports { get; set; }
         public virtual Park Park { get; set; }
         public int ParkId { get; set; }
+        public double Price { get; set; }
         public virtual Picture Picture { get; set; }
         public  ICollection<Reserve> Reserves { get; set; }
 
---
+++ Sports4All/Migrations/202001311800595_M002.cs
@@ -0,0 +1,18 @@
+﻿namespace Sports4All.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class M002 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Grounds", "Price", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Grounds", "Price");
+        }
+    }
+}
